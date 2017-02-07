2dcb4d64e74a2d1175e5c727bd83f0d130bd2ba0
Chris Marshall <chris@codegoalie.com>
2017-02-07 15:04:17 -0500

    Expose Chain values

--- vchain/chain.go
+++ vchain/chain.go
@@ -2,33 +2,33 @@ package vchain
 
 // Chain represents a block chain. It exposes functions for ease of use.
 type Chain struct {
-	currentBlock  Block
+	CurrentBlock  Block
 	blockCapacity int
-	blocks        map[string]Block
+	Blocks        map[string]Block
 }
 
 // NewChain returns a new Chain with the starter block initilized to the Capacity
 func NewChain(cap int) Chain {
 	return Chain{
-		currentBlock:  Block{Capacity: cap, Parent: "0000000000000000000000000000000000000000"},
+		CurrentBlock:  Block{Capacity: cap, Parent: "0000000000000000000000000000000000000000"},
 		blockCapacity: cap,
-		blocks:        map[string]Block{},
+		Blocks:        map[string]Block{},
 	}
 }
 
 // AddVote adds a vote to the current block on the chain. If he block is full,
 // the block will be added to the chain and a new currentBlock will be created.
 func (c *Chain) AddVote(v Vote) error {
-	err := c.currentBlock.AddVote(v)
+	err := c.CurrentBlock.AddVote(v)
 	if err != nil {
 		return err
 	}
 
-	if len(c.currentBlock.votes) >= c.blockCapacity {
-		blockHash := c.currentBlock.Hash()
-		c.blocks[blockHash] = c.currentBlock
-		c.currentBlock = Block{Number: c.currentBlock.Number + 1, Capacity: c.blockCapacity}
-		c.currentBlock.Parent = blockHash
+	if len(c.CurrentBlock.votes) >= c.blockCapacity {
+		blockHash := c.CurrentBlock.Hash()
+		c.Blocks[blockHash] = c.CurrentBlock
+		c.CurrentBlock = Block{Number: c.CurrentBlock.Number + 1, Capacity: c.blockCapacity}
+		c.CurrentBlock.Parent = blockHash
 	}
 
 	return nil
