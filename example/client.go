866173b1b9852cc9425ccfda4eb8b5634cd764f5
Chris Marshall <chris@codegoalie.com>
2017-02-12 08:52:59 -0500

    Change Coordination method to Mined
    gPRC calls are transactional in nature (req/resp) so note
    to node coordiation can take place in descrete messagest to peers not
    over long lived connections.

--- example/client.go
+++ example/client.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"io"
+	"context"
 	"log"
 
-	context "golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	pb "github.com/codegoalie/votechain/votechain"
@@ -18,7 +17,6 @@ func main() {
 	defer conn.Close()
 	client := pb.NewPollingStationClient(conn)
 
-	stream, err := client.Coordinate(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to get stream: %v", err)
 	}
@@ -36,32 +34,12 @@ func main() {
 		},
 	}
 
-	waitc := make(chan struct{})
-	go func() {
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				// read done.
-				close(waitc)
-				return
-			}
-			if err != nil {
-				log.Fatalf("Failed to receive a vote/block : %v", err)
-			}
-			log.Printf("Got message %+v\n", in.Transaction)
-		}
-	}()
 	for _, vote := range votes {
 		log.Printf("Sending vote: %v\n", vote)
-		err := stream.Send(&pb.Coordination{
-			Transaction: &pb.Coordination_Vote{
-				Vote: &vote,
-			}})
+		_, err := client.Cast(context.Background(), &vote)
 
 		if err != nil {
 			log.Fatalf("Failed to send a vote: %v", err)
 		}
 	}
-	stream.CloseSend()
-	<-waitc
 }
--- polling-place/server.go
+++ polling-place/server.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
 
@@ -60,51 +59,9 @@ func (s pollingPlaceServer) GetBlock(ctx context.Context, in *pb.BlockNumber) (*
 	return nil, errors.New("Unknown block")
 }
 
-func (s pollingPlaceServer) Coordinate(client pb.PollingStation_CoordinateServer) error {
-	for {
-		fmt.Println("Receiving")
-		in, err := client.Recv()
-		if err == io.EOF {
-			return nil
-
-		}
-		if err != nil {
-			return err
-
-		}
-
-		switch msg := in.Transaction.(type) {
-		case *pb.Coordination_Block:
-			block := msg.Block
-			if _, ok := s.chain.Blocks[block.Hash]; ok {
-				log.Printf("Coordinated block found: %s\n", block.Hash)
-			} else {
-				log.Printf("Coordinated block not found: %s\nNeed to figure out how to add this\n", block.Hash)
-			}
-		case *pb.Coordination_Vote:
-			vote := msg.Vote
-			log.Printf("Coordinated vote received: %+v\n", vote)
-			s.chain.AddVote(vchain.Vote{
-				VoterProof: vote.VoterProof,
-				RaceID:     int(vote.RaceId),
-				Selection:  vote.Selection,
-			})
-			log.Printf("Coordinated vote Added! \n%+v\n", s.chain)
-		}
-
-		log.Println("At newVotes select")
-		select {
-		case vote := <-s.newVotes:
-			log.Printf("Sending newVote: %+v\n", vote)
-			client.Send(&pb.Coordination{
-				Transaction: &pb.Coordination_Vote{
-					Vote: vote,
-				},
-			})
-		default:
-			log.Println("Skip newVotes")
-		}
-	}
+func (s pollingPlaceServer) Mined(ctx context.Context, msg *pb.Block) (*pb.Empty, error) {
+	// add block to chain
+	return nil, nil
 }
 
 func newServer() *pollingPlaceServer {
